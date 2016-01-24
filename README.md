dc5c6b2052f292189c36a515acf4239ab7f6fdc8
Junxiao Shi <git@mail1.yoursunny.com>
2016-01-24 05:26:37 -0700

    choose room

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# HomeCamSpark
+# HomeCamSpark - home surveillance camera with Cisco Spark
+
+## Setup
+
+1. `apt-get install php5-cli php5-curl`
+
+1. Download CiscoSpark on your mobile device, sign in with your personal account
+2. Sign in to <https://web.ciscospark.com> with a separate camera account
+3. create a room and invite your personal account
+4. Log in to <https://developer.ciscospark.com>, copy access token, and paste in accesstoken.txt
+5. Run `./setup.sh`
---
+++ choose-room.php
@@ -0,0 +1,34 @@
+<?php
+$accesstoken = file_get_contents('accesstoken.txt');
+
+$context = stream_context_create(array(
+  'http'=>array(
+    'method'=>'GET',
+    'header'=>'Authorization: Bearer '.$accesstoken
+  )
+));
+$json = file_get_contents('https://api.ciscospark.com/v1/rooms', false, $context);
+$j = json_decode($json);
+
+echo "Available Rooms:\n";
+for ($i = 0; $i < count($j->items); ++$i) {
+  printf("%d. %s\n", $i, $j->items[$i]->title);
+}
+echo "\n";
+
+$sel = -1;
+$stdin = fopen('php://stdin', 'r');
+while ($sel < 0 || $sel >= count($j->items)) {
+  printf("Choose a room [0-%d]: ", count($j->items) - 1);
+  $line = trim(fgets($stdin));
+  $sel = intval($line);
+  if (strval($sel) != $line) {
+    $sel = -1;
+  }
+}
+fclose($stdin);
+
+$f = fopen('room.txt', 'w');
+fwrite($f, $j->items[$sel]->id);
+fclose($f);
+?>
